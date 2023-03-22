60dd5b47eeac4dc1e9b59d2aa8fffb2b0dec0863
sarvagna <sarvagnakadiya1@gmail.com>
2023-03-23 00:29:35 +0530

    hardhat polyscan verification added

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,14 +3,17 @@ require("dotenv").config({ path: __dirname + "/.env" });
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
-  paths: {
-    artifacts: "./src/artifacts",
-  },
+  solidity: "0.8.18",
+  // paths: {
+  //   artifacts: "./src/artifacts",
+  // },
   networks: {
     mumbai: {
       url: process.env.API_KEY_URL, //Your RPC URL
       accounts: [process.env.PRIVATE_KEY], //Your private key
     },
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY,
   },
 };
