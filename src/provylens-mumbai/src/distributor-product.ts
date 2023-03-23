bc72af20cc5018eaf0b08966eae020b0fd6d4e66
sarvagna <sarvagnakadiya1@gmail.com>
2023-03-23 23:00:32 +0530

    graph error

--- src/provylens-mumbai/src/distributor-product.ts
+++ src/provylens-mumbai/src/distributor-product.ts
@@ -1,36 +1,39 @@
+import { Bytes } from "@graphprotocol/graph-ts";
 import {
   eventAddDistributorProduct as eventAddDistributorProductEvent,
   eventDeleteDistributorProduct as eventDeleteDistributorProductEvent,
-  eventUpdateDistributorProductUints as eventUpdateDistributorProductUintsEvent
-} from "../generated/distributorProduct/distributorProduct"
+  eventUpdateDistributorProductUints as eventUpdateDistributorProductUintsEvent,
+} from "../generated/distributorProduct/distributorProduct";
 import {
   eventAddDistributorProduct,
   eventDeleteDistributorProduct,
-  eventUpdateDistributorProductUints
-} from "../generated/schema"
+  eventUpdateDistributorProductUints,
+} from "../generated/schema";
 
 export function handleeventAddDistributorProduct(
   event: eventAddDistributorProductEvent
 ): void {
   let entity = new eventAddDistributorProduct(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._dpId = event.params._dpId
-  entity._manufacturerAddress = event.params._manufacturerAddress
-  entity._mdId = event.params._mdId
-  entity._name = event.params._name
-  entity._description = event.params._description
-  entity._unit = event.params._unit
-  entity._price = event.params._price
-  entity._date = event.params._date
-  entity._expiryDate = event.params._expiryDate
-  entity.status = event.params.status
+  );
+  entity._dpId = event.params._dpId;
+  entity._manufacturerAddress = event.params._manufacturerAddress.map<Bytes>(
+    (e: Bytes) => e
+  );
+  entity._mdId = event.params._mdId.map<Bytes>((e: Bytes) => e);
+  entity._name = event.params._name;
+  entity._description = event.params._description;
+  entity._unit = event.params._unit;
+  entity._price = event.params._price;
+  entity._date = event.params._date;
+  entity._expiryDate = event.params._expiryDate;
+  entity.status = event.params.status;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleeventDeleteDistributorProduct(
@@ -38,14 +41,14 @@ export function handleeventDeleteDistributorProduct(
 ): void {
   let entity = new eventDeleteDistributorProduct(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._id = event.params._id
+  );
+  entity._id = event.params._id;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleeventUpdateDistributorProductUints(
@@ -53,13 +56,13 @@ export function handleeventUpdateDistributorProductUints(
 ): void {
   let entity = new eventUpdateDistributorProductUints(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._dpId = event.params._dpId
-  entity._quantity = event.params._quantity
+  );
+  entity._dpId = event.params._dpId;
+  entity._quantity = event.params._quantity;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
--- src/provylens-mumbai/src/manufacturer-product.ts
+++ src/provylens-mumbai/src/manufacturer-product.ts
@@ -1,36 +1,39 @@
+import { Bytes } from "@graphprotocol/graph-ts";
 import {
   eventAddManufacturerProduct as eventAddManufacturerProductEvent,
   eventDeleteManufacturerProduct as eventDeleteManufacturerProductEvent,
-  manufacturerProducteventUpdateDistributorProductUints as manufacturerProducteventUpdateDistributorProductUintsEvent
-} from "../generated/manufacturerProduct/manufacturerProduct"
+  manufacturerProducteventUpdateDistributorProductUints as manufacturerProducteventUpdateDistributorProductUintsEvent,
+} from "../generated/manufacturerProduct/manufacturerProduct";
 import {
   eventAddManufacturerProduct,
   eventDeleteManufacturerProduct,
-  manufacturerProducteventUpdateDistributorProductUints
-} from "../generated/schema"
+  manufacturerProducteventUpdateDistributorProductUints,
+} from "../generated/schema";
 
 export function handleeventAddManufacturerProduct(
   event: eventAddManufacturerProductEvent
 ): void {
   let entity = new eventAddManufacturerProduct(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._mpId = event.params._mpId
-  entity._supplierAddress = event.params._supplierAddress
-  entity._smId = event.params._smId
-  entity._name = event.params._name
-  entity._description = event.params._description
-  entity._unit = event.params._unit
-  entity._price = event.params._price
-  entity._date = event.params._date
-  entity._expiryDate = event.params._expiryDate
-  entity.status = event.params.status
+  );
+  entity._mpId = event.params._mpId;
+  entity._supplierAddress = event.params._supplierAddress.map<Bytes>(
+    (e: Bytes) => e
+  );
+  entity._smId = event.params._smId.map<Bytes>((e: Bytes) => e);
+  entity._name = event.params._name;
+  entity._description = event.params._description;
+  entity._unit = event.params._unit;
+  entity._price = event.params._price;
+  entity._date = event.params._date;
+  entity._expiryDate = event.params._expiryDate;
+  entity.status = event.params.status;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleeventDeleteManufacturerProduct(
@@ -38,14 +41,14 @@ export function handleeventDeleteManufacturerProduct(
 ): void {
   let entity = new eventDeleteManufacturerProduct(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._Id = event.params._Id
+  );
+  entity._Id = event.params._Id;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handlemanufacturerProducteventUpdateDistributorProductUints(
@@ -53,13 +56,13 @@ export function handlemanufacturerProducteventUpdateDistributorProductUints(
 ): void {
   let entity = new manufacturerProducteventUpdateDistributorProductUints(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._dpId = event.params._dpId
-  entity._quantity = event.params._quantity
+  );
+  entity._dpId = event.params._dpId;
+  entity._quantity = event.params._quantity;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
