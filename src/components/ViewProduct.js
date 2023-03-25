9a92642d7af2ad683d5ee68c6de81360df5310c1
Lajja <lajja.lamprostech@gmail.com>
2023-03-25 16:20:15 +0530

    add view product page

--- src/components/ViewProduct.js
+++ src/components/ViewProduct.js
@@ -1,59 +1,129 @@
 import React, { useRef, useState } from "react";
 import "../styles/deleteproduct.css";
+import productData from "./ViewProduct.json";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import { styled } from "@mui/material/styles";
+import "../styles/viewproduct.css";
+// ................
 import Button from "@mui/material/Button";
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import Select from "@mui/material/Select";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+
+// ..............
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:nth-of-type(odd)": {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  "&:last-child td, &:last-child th": {
+    border: 0,
+  },
+}));
+
+function createData(id, name, unit, price) {
+  return { id, name, unit, price };
+}
+
+const rows = [createData("1", "Apollo", 500, 1000)];
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 
 function ViewProduct() {
+  const [modal, setModal] = useState(false);
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
+  if (modal) {
+    document.body.classList.add("active-modal");
+  } else {
+    document.body.classList.remove("active-modal");
+  }
   return (
     <>
-      <div className="availabel-proposal-main-div">
-        <div className="delete-product-main-div">
-          <FormControl
-            sx={{ m: 1, minWidth: 70 }}
-            size="small"
-            id="dropdown-formcontrol"
-            className="select-parent"
-          >
-            <InputLabel id="select-label-status">Product Id</InputLabel>
-            <Select
-              labelId="demo-select-small"
-              id="demo-select-small"
-              // value={age}
-              label="Product-Id"
-              // onChange={handleChange}
-            >
-              <MenuItem value="">
-                <em>None</em>
-              </MenuItem>
-              <MenuItem value={10}>1 </MenuItem>
-              <MenuItem value={20}>2 </MenuItem>
-              <MenuItem value={30}>3</MenuItem>
-            </Select>
-          </FormControl>
-          <Button
-            onClick={toastInfo}
-            variant="contained"
-            size="large"
-            className="delete-btn"
-          >
-            Delete
-          </Button>
-
-          <ToastContainer
-            position="bottom-right"
-            autoClose={5000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-          />
+      {modal && (
+        <div className=" modal ">
+          <div onClick={toggleModal} className="overlay"></div>
+          <div className=" modal-content">
+            <div className="first-row">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum."
+              </p>
+              <button className="close-modal" onClick={toggleModal}>
+                CLOSE
+              </button>
+            </div>
+          </div>
         </div>
+      )}
+      <div className="availabel-proposal-main-div">
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 700 }} aria-label="customized table">
+            <TableHead>
+              <TableRow>
+                <StyledTableCell>Product Id</StyledTableCell>
+                <StyledTableCell align="right">Product Id</StyledTableCell>
+                <StyledTableCell align="right">Product name</StyledTableCell>
+                <StyledTableCell align="right">Unit</StyledTableCell>
+                <StyledTableCell align="right"></StyledTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <StyledTableRow key={row.name}>
+                  <StyledTableCell component="th" scope="row">
+                    {row.id}
+                  </StyledTableCell>
+                  <StyledTableCell align="right">{row.name}</StyledTableCell>
+                  <StyledTableCell align="right">{row.unit}</StyledTableCell>
+                  <StyledTableCell align="right">{row.price}</StyledTableCell>
+                  <div>
+                    <Button
+                      variant="contained"
+                      size="large"
+                      className="view-More"
+                      onClick={toggleModal}
+                      // onClick={() => {
+                      //   dashboardLinks("HistoryDetails");
+                      // }}
+                    >
+                      View More
+                    </Button>
+                  </div>
+                </StyledTableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </div>
     </>
   );
